1997f1029131f4b1159824cd5290daad3053092f
Derick <derick.v.silva@edu.senai.br>
2026-04-23 12:04:01 -0300

    v.0.3 - adcionado função de subtrair ao projeto

--- main.php
+++ main.php
@@ -1,8 +1,20 @@
 <?php
-echo "Na aula do dia 23/04/2026 eu aprendi a mexer no GitHub/git e desenvolver funções no PHP =D";
-function soma($a, $b){
+
+function soma($a, $b)
+{
     return $a + $b;
 }
+
+function subtrair($a, $b){
+
+    return $a - $b;
+}
+
+// commit v3
+
+//criar função dividir (v4) e multiplicar (v5)
+
+echo "resultado:", subtrair(11,6);
 
 
 
