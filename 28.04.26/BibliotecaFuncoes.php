60bb8168ff20c63abff415d6e9bcf23994dd0def
Derick <derick.v.silva@edu.senai.br>
2026-04-28 10:30:36 -0300

    v.0.9 - adcionado calcúlo de áreas

--- 28.04.26/BibliotecaFuncoes.php
+++ 28.04.26/BibliotecaFuncoes.php
@@ -1,6 +1,7 @@
 <?php 
 
 namespace conversoes{
+
   function DolarParaReal ($valor, $CotacaoDolar){
     return $valor * $CotacaoDolar;
   }
@@ -20,5 +21,29 @@ namespace conversoes{
   function IeneParaReal ($valor, $CotacaoIene){
     return $valor * $CotacaoIene;
   }
-}
+  }
+
+
+namespace areas{
+  function areaQuadrado ($lado){
+    return $lado * $lado;
+  }
+
+  function areaRetangulo($base, $altura){
+    return $base * $altura;
+  }
+
+  function areaTriangulo($base, $altura){
+    return $base * $altura / 2;
+  }
+    
+  function areaCirculo($raio){
+    return 3.14 * $raio * $raio;
+  }
+
+  function areaTrapezio($baseMaior, $baseMenor, $altura){
+    return ($baseMaior + $baseMenor) * $altura / 2;
+  }
+  }
+
 ?>
--- 28.04.26/EntradaDeDados.php
+++ 28.04.26/EntradaDeDados.php
@@ -2,21 +2,23 @@
 
 require_once "BibliotecaFuncoes.php";
 
+///conversões 
 use function conversoes\DolarParaReal;
 use function conversoes\EuroParaReal;
 use function conversoes\PesoParaReal;
 use function conversoes\LibraParaReal;
 use function conversoes\IeneParaReal;
 
-echo "O seu valor do dolár em real é: ", DolarParaReal(1,5), "\n";
+///Cálculo de área
+use function areas\areaQuadrado;
+use function areas\areaRetangulo;
+use function areas\areaTriangulo;
+use function areas\areaCirculo;
+use function areas\areaTrapezio;
 
-echo "O seu valor do euro em real é: ", EuroParaReal(1,5.85), "\n";
-
-echo "O seu valor do peso em real é: ", PesoParaReal(1,0.0035), "\n";
-
-echo "O seu valor da libra em real é: ", LibraParaReal(1,6.74), "\n";
-
-echo "O seu valor do iene em real é: ", IeneParaReal(1,0.031), "\n";
-
- 
+echo "A área do quadrado é de ", areaQuadrado(12), "m²", "\n";
+echo "A área do retângulo é de ",areaRetangulo(9,6), "m²", "\n";
+echo "A área do triângulo é de ", areaTriangulo(8,7), "m²", "\n";
+echo "A área do círculo é de ", areaCirculo(10), "m²", "\n";
+echo "A área do trapézio é de ", areaTrapezio(7,5,8), "m²", "\n";
 ?>
